e1d2809812d8a7d64d2adb7844eb32f969dc1483
Pere Soler <erep68@gmail.com>
2017-06-06 15:48:57 +0200

    Primer commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    Article = require('./models/article');
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  Articles.find({}, (err, articles) => {
+    if(err){
+      console.log(err);
+    }else{
+      res.render('index', {
+        title: 'Listado de articulos',
+        articles: articles
+      });
+    }
+  });
+});
+
+module.exports = router;
